1b54842b233ebc29042bd728e8eb373224145695
Alexandar Mechev <apmechev@gmail.com>
2016-04-21 13:02:01 +0200

    Sample 100k test of start-stop of all 5 timers

--- main.cpp
+++ main.cpp
@@ -11,12 +11,26 @@ std::string
 getFileContent(const std::string& path)
 {
     GUTimer t(GUTimer::LOFAR_timer);// GUTimer t(GUTimer::time_h)
+    GUTimer t1(GUTimer::rdtsc);// GUTimer t(GUTimer::time_h)
+    GUTimer t2(GUTimer::time_h);// GUTimer t(GUTimer::time_h)
+    GUTimer t3(GUTimer::chrono_hr);// GUTimer t(GUTimer::time_h)
+    GUTimer t4(GUTimer::ctime);// GUTimer t(GUTimer::time_h)
     t.start();
+    t.stop();
+    t1.start();
+    t1.stop();
+    t2.start();
+    t2.stop();
+    t3.start();
+    t3.stop();
+    t4.start();
+    t4.stop();
 
   std::ifstream file(path);
   std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 
-    t.stop();std::cout<<t.getElapsed()<<"\n";
+    //t.stop();
+    std::cout<<t.getElapsed()<<" "<<t1.getElapsed()<<" "<<t2.getElapsed()<<" "<<t3.getElapsed()<<" "<<t4.getElapsed()<<"\n";
 
   return "";
 }
@@ -24,7 +38,7 @@ getFileContent(const std::string& path)
 
 
 int main() {
-for(int i=0;i<=100;i++){
+for(int i=0;i<=100000;i++){
 getFileContent("/proc/loadavg");
 }
 return 0;}
